cff194ac7e47e5d4cda07ab9c5affd133dabde94
Eliza Garnica <Eliza Garnica@ENVY>
2022-02-03 15:55:16 -0600

    Add project files.

---
+++ PrimerAPI/Friend.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Friend
+{
+	private string firstname { get; set; }
+	private string lastname { get; set; }
+	private string location { get; set; }
+	private DateTime dateOfHire { get; set; }
+	public Friend()
+	{
+	}
+	public Friend (string firstname, string lastname, string location, DateTime dateOfHire)
+    {
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.location = location;
+		this.dateOfHire = dateOfHire;
+    }
+}
